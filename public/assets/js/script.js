9f125a303aa526ce666ef96b7e2c0fd5ac65cb41
alecvipa <hebrit_626@hotmail.com>
2022-11-28 14:44:16 +0300

    added new features

---
+++ public/assets/js/script.js
@@ -0,0 +1,12 @@
+function navB() {
+    if(window.scrollY==0){
+        //user scrolled to the top of the page
+        document.getElementById("nav").style.backgroundColor = "white" ;
+    }
+    else{
+
+        document.getElementById("nav").style.backgroundColor = "#b80605" ;
+    }
+    
+}
+
